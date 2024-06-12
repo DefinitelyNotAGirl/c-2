4f0b897791b1a9b29c0c7df67057f16bc5ac3e4e
DefinitelyNotAGirl <definitelynotagirl115169@gmail.com>
2024-06-12 05:59:59 +0200

    added RelocationEntry and RelocationType classes

--- inc/ELF64.hxx
+++ inc/ELF64.hxx
@@ -99,5 +99,58 @@ namespace elf64
 		constexpr uint64_t mask_os			= 0x0FF00000;
 		constexpr uint64_t mask_proc		= 0xF0000000;
 	}
+
+	enum class RelocationType : uint32_t {
+		R_X86_64_NONE			= 0,	//+ No relocation
+		R_X86_64_64				= 1,	//+ Direct 64-bit
+		R_X86_64_PC32			= 2,	//+ PC relative 32-bit signed
+		R_X86_64_GOT32			= 3,	//+ 32-bit GOT entry
+		R_X86_64_PLT32			= 4,	//+ 32-bit PLT address
+		R_X86_64_COPY			= 5,	//+ Copy symbol at runtime
+		R_X86_64_GLOB_DAT		= 6,	//+ Create GOT entry
+		R_X86_64_JUMP_SLOT		= 7,	//+ Create PLT entry
+		R_X86_64_RELATIVE		= 8,	//+ Adjust by program base
+		R_X86_64_GOTPCREL		= 9,	//+ 32-bit signed PC relative offset to GOT
+		R_X86_64_32				= 10,	//+ Direct 32-bit zero-extended
+		R_X86_64_32S			= 11,	//+ Direct 32-bit sign-extended
+		R_X86_64_16				= 12,	//+ Direct 16-bit zero-extended
+		R_X86_64_PC16			= 13,	//+ 16-bit sign-extended PC relative
+		R_X86_64_8				= 14,	//+ Direct 8-bit sign-extended
+		R_X86_64_PC8			= 15,	//+ 8-bit sign-extended PC relative
+		R_X86_64_DTPMOD64		= 16,	//+ ID of module containing symbol
+		R_X86_64_DTPOFF64		= 17,	//+ Offset in module's TLS block
+		R_X86_64_TPOFF64		= 18,	//+ Offset in initial TLS block
+		R_X86_64_TLSGD			= 19,	//+ 32-bit signed PC relative offset to two GOT entries for GD symbol
+		R_X86_64_TLSLD			= 20,	//+ 32-bit signed PC relative offset to two GOT entries for LD symbol
+		R_X86_64_DTPOFF32		= 21,	//+ Offset in TLS block
+		R_X86_64_GOTTPOFF		= 22,	//+ 32-bit signed PC relative offset to GOT entry for IE symbol
+		R_X86_64_TPOFF32		= 23,	//+ Offset in initial TLS block
+		R_X86_64_PC64			= 24,	//+ PC relative 64-bit
+		R_X86_64_GOTOFF64		= 25,	//+ 64-bit offset to GOT
+		R_X86_64_GOTPC32		= 26,	//+ 32-bit signed PC relative offset to GOT
+		R_X86_64_GOT64			= 27,	//+ 64-bit GOT entry offset
+		R_X86_64_GOTPCREL64		= 28,	//+ 64-bit PC relative offset to GOT entry
+		R_X86_64_GOTPC64		= 29,	//+ 64-bit PC relative offset to GOT
+		R_X86_64_GOTPLT64		= 30,	//+ Like GOT64, says PLT entry needed
+		R_X86_64_PLTOFF64		= 31,	//+ 64-bit GOT relative offset to PLT entry
+		R_X86_64_SIZE32			= 32,	//+ Size of symbol plus 32-bit addend
+		R_X86_64_SIZE64			= 33,	//+ Size of symbol plus 64-bit addend
+		R_X86_64_GOTPC32_TLSDESC= 34,	//+ GOT offset for TLS descriptor.
+		R_X86_64_TLSDESC_CALL	= 35,	//+ Marker for call through TLS descriptor.
+		R_X86_64_TLSDESC		= 36,	//+ TLS descriptor.
+		R_X86_64_IRELATIVE		= 37,	//+ Adjust indirectly by program base
+		R_X86_64_RELATIVE64		= 38,	//+ 64-bit adjust by program base
+		R_X86_64_GOTPCRELX		= 41,	//+ Load from 32 bit signed pc relative offset to GOT
+		R_X86_64_REX_GOTPCRELX	= 42,	//+ Load from 32 bit signed pc relative offset to GOT under REX
+		R_X86_64_NUM			= 43	//+ Keep this the last entry
+	};
+
+	struct RelocationEntry {
+		uint64_t offset;
+		uint64_t info;
+		int64_t addend;
+		RelocationEntry(uint64_t offset, uint32_t symbol, RelocationType rtype, int64_t addend)
+			:offset(offset),info(symbol<<32 | rtype),addend(addend){}
+	};
 	#pragma pack()
 }
