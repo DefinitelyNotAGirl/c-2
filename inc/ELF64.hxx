079b2a3f44d399f132153994056216d538865cfd
DefinitelyNotAGirl <definitelynotagirl115169@gmail.com>
2024-06-12 08:20:02 +0200

    added SectionHeader and ProgramHeader enums

--- inc/ELF64.hxx
+++ inc/ELF64.hxx
@@ -145,6 +145,79 @@ namespace elf64
 		R_X86_64_NUM			= 43	//+ Keep this the last entry
 	};
 
+	enum class SectionType : uint32_t
+	{
+		SHT_NULL				= 0,          // Section header table entry unused
+		SHT_PROGBITS			= 1,          // Program data
+		SHT_SYMTAB				= 2,          // Symbol table
+		SHT_STRTAB				= 3,          // String table
+		SHT_RELA				= 4,          // Relocation entries with addends
+		SHT_HASH				= 5,          // Symbol hash table
+		SHT_DYNAMIC				= 6,          // Dynamic linking information
+		SHT_NOTE				= 7,          // Notes
+		SHT_NOBITS				= 8,          // Program space with no data (bss)
+		SHT_REL					= 9,          // Relocation entries, no addends
+		SHT_SHLIB				= 10,         // Reserved
+		SHT_DYNSYM				= 11,         // Dynamic linker symbol table
+		SHT_INIT_ARRAY			= 14,         // Array of constructors
+		SHT_FINI_ARRAY			= 15,         // Array of destructors
+		SHT_PREINIT_ARRAY		= 16,         // Array of pre-constructors
+		SHT_GROUP				= 17,         // Section group
+		SHT_SYMTAB_SHNDX		= 18,         // Extended section indices
+		SHT_NUM					= 19,         // Number of defined types.
+		SHT_LOOS				= 0x60000000, // Start OS-specific.
+		SHT_GNU_ATTRIBUTES		= 0x6ffffff5, // Object attributes.
+		SHT_GNU_HASH			= 0x6ffffff6, // GNU-style hash table.
+		SHT_GNU_LIBLIST			= 0x6ffffff7, // Prelink library list
+		SHT_CHECKSUM			= 0x6ffffff8, // Checksum for DSO content.
+		SHT_LOSUNW				= 0x6ffffffa, // Sun-specific low
+		SHT_SUNW_move			= 0x6ffffffa,
+		SHT_SUNW_COMDAT			= 0x6ffffffb,
+		SHT_SUNW_syminfo		= 0x6ffffffc,
+		SHT_GNU_verdef			= 0x6ffffffd, // Version definition section.
+		SHT_GNU_verneed			= 0x6ffffffe, // Version needs section.
+		SHT_GNU_versym			= 0x6fffffff, // Version symbol table.
+		SHT_HISUNW				= 0x6fffffff, // Sun-specific high
+		SHT_HIOS				= 0x6fffffff, // End OS-specific type
+		SHT_LOPROC				= 0x70000000, // Start of processor-specific
+		SHT_HIPROC				= 0x7fffffff, // End of processor-specific
+		SHT_LOUSER				= 0x80000000, // Start of application-specific
+		SHT_HIUSER				= 0x8fffffff  // End of application-specific
+	};
+	inline uint32_t to_uint32(SectionType type)
+	{
+		return static_cast<uint32_t>(type);
+	}
+
+	enum class ProgramHeaderType : uint32_t
+	{
+		PT_NULL				= 0,          // Program header table entry unused
+		PT_LOAD				= 1,          // Loadable program segment
+		PT_DYNAMIC			= 2,          // Dynamic linking information
+		PT_INTERP			= 3,          // Program interpreter
+		PT_NOTE				= 4,          // Auxiliary information
+		PT_SHLIB			= 5,          // Reserved
+		PT_PHDR				= 6,          // Entry for header table itself
+		PT_TLS				= 7,          // Thread-local storage segment
+		PT_NUM				= 8,          // Number of defined types
+		PT_LOOS				= 0x60000000, // Start of OS-specific
+		PT_GNU_EH_FRAME		= 0x6474e550, // GCC .eh_frame_hdr segment
+		PT_GNU_STACK		= 0x6474e551, // Indicates stack executability
+		PT_GNU_RELRO		= 0x6474e552, // Read-only after relocation
+		PT_LOSUNW			= 0x6ffffffa, // Sun-specific low
+		PT_SUNWBSS			= 0x6ffffffa, // Sun-specific bss
+		PT_SUNWSTACK		= 0x6ffffffb, // Sun-specific stack
+		PT_HISUNW			= 0x6fffffff, // Sun-specific high
+		PT_HIOS				= 0x6fffffff, // End of OS-specific
+		PT_LOPROC			= 0x70000000, // Start of processor-specific
+		PT_HIPROC			= 0x7fffffff  // End of processor-specific
+	};
+	
+	inline uint32_t to_uint32(ProgramHeaderType type)
+	{
+		return static_cast<uint32_t>(type);
+	}
+
 	struct RelocationEntry {
 		uint64_t offset;
 		uint64_t info;
