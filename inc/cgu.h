d6c52ad1da22e843f6167d83f4c1a821276d1a52
DefinitelyNotAGirl <definitelynotagirl115169@gmail.com>
2024-06-14 09:51:17 +0200

    added a `SectionIndex` Field to cgu::Symbol

--- inc/cgu.h
+++ inc/cgu.h
@@ -5,10 +5,14 @@
 namespace cgu
 {
 	class Symbol {
+	public:
+		uint64_t SectionIndex;
 		std::string text;
 		uint64_t size;
 		uint64_t offset;
 		bool defined;
+		Symbol(std::string text, uint64_t size, uint64_t offset, bool defined, uint64_t SectionIndex = 0xFFF1)
+			:text(text),size(size),offset(offset),defined(defined),SectionIndex(SectionIndex){}
 	};
 
 	std::vector<Symbol> symbols;
