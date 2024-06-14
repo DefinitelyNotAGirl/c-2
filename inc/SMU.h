fd943d929d08cc81e84fe19bde0bcc15c87d454d
DefinitelyNotAGirl <definitelynotagirl115169@gmail.com>
2024-06-14 13:39:27 +0200

    added array push overload to smu::section

--- inc/SMU.h
+++ inc/SMU.h
@@ -172,6 +172,18 @@ namespace smu
 		 */
 		void push(std::initializer_list<byte> data){*this << data;}
 		/**
+		 * @brief adds data[0 through (n-1)] to the current data
+		 * 
+		 */
+		template<typename T>
+		void push(T* data, uint64_t n)
+		{
+			uint64_t newSize = this->sizeInFile+(n*sizeof(T));
+			this->data = (byte*)realloc(this->data,newSize);
+			memcpy(this->data+this->sizeInFile,&data,(n*sizeof(T)));
+			this->sizeInFile = newSize;
+		}
+		/**
 		 * @brief adds data to the current data
 		 * 
 		 */
