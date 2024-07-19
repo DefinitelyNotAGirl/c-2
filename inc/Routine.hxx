1a578750db2f3c191f269ef0984f98f5737261d7
DefinitelyNotAGirl <definitelynotagirl115169@gmail.com>
2024-07-20 01:00:29 +0200

    added Routine class

---
+++ inc/Routine.hxx
@@ -0,0 +1,18 @@
+#pragma once
+
+typedef void(*RoutineCallback)(void*);
+class Routine {
+public:
+	void* data;
+	RoutineCallback callback;
+
+	Routine(void* data,RoutineCallback callback)
+		:callback(callback),data(data){}
+
+	Routine(RoutineCallback callback)
+		:callback(callback),data(nullptr){}
+
+	void run(){
+		this->callback(this->data);
+	}
+}
