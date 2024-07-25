f0975c5764386c98e0615952c4a6fe4cd714d9aa
DefinitelyNotAGirl <definitelynotagirl115169@gmail.com>
2024-07-25 21:40:46 +0200

    streamlined things in run_debug.py

--- run_debug.py
+++ run_debug.py
@@ -4,18 +4,26 @@ def execute(cmd: str):
 	if os.system(cmd) != 0:
 		exit (1)
 
+def CreateDirectory(path: str):
+	os.system("mkdir "+path)
+
+def DeleteFile(path: str):
+	os.system("rm "+path)
+
+def CMDtoHTML(cmd: str,dst: str):
+	execute(cmd+" &> "+dst)
+	execute("ansiToHtml "+dst)
+	DeleteFile(dst)
+
+def dumpELF(file: str,dir: str):
+	CreateDirectory(dir)
+	CMDtoHTML("eu-elflint "+file,dir+"/eu-elflint")
+	CMDtoHTML("eu-readelf -a "+file,dir+"/eu-readelf")
+	CMDtoHTML("readelf -a "+file,dir+"/gnu-readelf")
+	CMDtoHTML("objdump -M intel -M x86-64 -M amd64 --disassembler-color=extended --visualize-jumps=extended-color --wide --show-all-symbols -d "+file,dir+"/disassembly")
+	return
+
 execute("python ./build-system/main.py debug")
-if os.system("make -C ../cpe2Example C2ARGS=\"-Wno-unimplemented -Wno-deprecated\"") != 0:
-	os.system("eu-elflint ../cpe2Example/build/main.o &> debug/elflint.ans")
-	os.system("ansiToHtml debug/elflint.ans")
-	os.system("eu-readelf -a ../cpe2Example/build/main.o &> debug/readelf.ans")
-	os.system("ansiToHtml debug/readelf.ans")
-	exit (1)
-execute("eu-elflint ../cpe2Example/build/main.o &> debug/elflint.ans")
-execute("ansiToHtml debug/elflint.ans")
-execute("eu-readelf -a ../cpe2Example/build/main.o &> debug/readelf.ans")
-execute("ansiToHtml debug/readelf.ans")
-execute("objdump -M intel -M x86-64 -M amd64 --disassembler-color=extended --visualize-jumps=extended-color --wide --show-all-symbols -d ../cpe2Example/build/main.o &> debug/main.o.disas")
-execute("ansiToHtml debug/main.o.disas")
-execute("objdump -M intel -M x86-64 -M amd64 --disassembler-color=extended --visualize-jumps=extended-color --wide --show-all-symbols -d ../cpe2Example/test.exe &> debug/test.exe.disas")
-execute("ansiToHtml debug/test.exe.disas")
+os.system("make -C ../cpe2Example C2ARGS=\"-Wno-unimplemented -Wno-deprecated\"")
+dumpELF("../cpe2Example/build/main.o","debug/main.o")
+dumpELF("../cpe2Example/test.exe","debug/test.exe")
